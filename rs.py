23f19a5de7df356418945f1ce8ee297561fd981e
dgbogi564 <82741370+dgbogi564@users.noreply.github.com>
2022-02-23 12:44:44 -0500

    moved send() and recv() to submission files

--- rs.py
+++ rs.py
@@ -2,8 +2,8 @@ import socket
 import errno
 import select
 import sys
-from sock_helper import TimeoutError, send, recv
 import re
+
 
 def rs(rs_hostname=None, rs_listenport=None, ts1_hostname=None, ts1_listenport=None, ts2_hostname=None, ts2_listenport=None):
     error = None
@@ -82,6 +82,53 @@ def rs(rs_hostname=None, rs_listenport=None, ts1_hostname=None, ts1_listenport=N
     if error:
         raise
     exit()
+
+
+class TimeoutError(IOError):
+    def __init__(self):
+        self.errno = errno.ETIMEDOUT
+        self.message = 'Timeout occurred'
+
+    def __str__(self):
+        return self.message
+
+
+def send(sockets, data, timeout):
+    data += '\0'
+    for sock in sockets:
+        sent = 0
+        while len(data[sent:]):
+            try:
+                sent += sock.send(data[sent:])
+            except socket.error as e:
+                if e.errno != errno.EAGAIN:
+                    raise e
+                if not select.select([], [sock], [], timeout)[1]:
+                    raise TimeoutError()
+
+
+def recv(sockets, timeout):
+
+    # check if timeout occurred
+    ready = select.select(sockets, [], [], timeout)[0]
+    if not ready:
+        raise TimeoutError()
+
+    # receive data
+    sock = ready[0]
+    data = ''
+    while True:
+        try:
+            buf = sock.recv(100)
+            data += buf if buf else ''
+            if not buf or '\0' in buf:
+                data = data.rstrip('\0').strip()
+                break
+        except socket.error as e:
+            if e.errno != errno.EAGAIN:
+                raise e
+            select.select([sock], [], [], timeout)
+    return data, sock
 
 
 if __name__ == "__main__":
--- ts1.py
+++ ts1.py
@@ -2,7 +2,6 @@ import errno
 import socket
 import select
 import sys
-from sock_helper import TimeoutError, send, recv
 import re
 
 
@@ -54,6 +53,53 @@ def ts1(ts1_hostname=None, ts1_listenport=None):
     if error:
         raise
     exit()
+
+
+class TimeoutError(IOError):
+    def __init__(self):
+        self.errno = errno.ETIMEDOUT
+        self.message = 'Timeout occurred'
+
+    def __str__(self):
+        return self.message
+
+
+def send(sockets, data, timeout):
+    data += '\0'
+    for sock in sockets:
+        sent = 0
+        while len(data[sent:]):
+            try:
+                sent += sock.send(data[sent:])
+            except socket.error as e:
+                if e.errno != errno.EAGAIN:
+                    raise e
+                if not select.select([], [sock], [], timeout)[1]:
+                    raise TimeoutError()
+
+
+def recv(sockets, timeout):
+
+    # check if timeout occurred
+    ready = select.select(sockets, [], [], timeout)[0]
+    if not ready:
+        raise TimeoutError()
+
+    # receive data
+    sock = ready[0]
+    data = ''
+    while True:
+        try:
+            buf = sock.recv(100)
+            data += buf if buf else ''
+            if not buf or '\0' in buf:
+                data = data.rstrip('\0').strip()
+                break
+        except socket.error as e:
+            if e.errno != errno.EAGAIN:
+                raise e
+            select.select([sock], [], [], timeout)
+    return data, sock
 
 
 if __name__ == "__main__":
